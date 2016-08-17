016fb29e4c0c390bc67e96de6431fb8ca86b6029
Kamil S Jaroň <376090@mail.muni.cz>
2016-08-17 22:08:50 +0200

    twidth function fix (bracket problem)

--- R/twidth.R
+++ R/twidth.R
@@ -14,7 +14,7 @@
 # TODO: header
 # comment: theoretical width. sigma^2=dispersal; s=selection
 
-twidth = function(s, sigma = sqrt(0.5) {
-	fact = sqrt(8))
+twidth = function(s, sigma = sqrt(0.5)){
+	fact = sqrt(8)
 	return((fact * sigma) / sqrt(s))
 }
