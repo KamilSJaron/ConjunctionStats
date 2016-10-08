956a835d5baa7ca07f8700b30d54974346963023
Kamil S Jaroň <376090@mail.muni.cz>
2016-10-08 23:02:02 +0200

    getIntegratedSelection > getAUFC - area under fitness curve, corrected (thesis has wrong equation)

---
+++ R/getAUFC.R
@@ -0,0 +1,22 @@
+#' @title getAUFC
+#'
+#' @description
+#' \code{getAUFC} computes Area Under Fitness Curve
+#' defined by the Barton's fitness function 1 - s (4x (1 - x)^beta)
+#' where, s is selection against the most admixed individuals and
+#' beta is setrength of epistasic effect
+#'
+#' @param selection
+#'
+#' @param beta
+#'
+#' @author Kamil Jaron \email{kamiljaron at gmail.com}
+#'
+#' @export
+
+getAUFC <- function(selection, beta){
+  return( 1 -
+          (4^beta * selection * gamma(beta + 1)^2) /
+          (gamma(2 * beta + 2))
+        )
+}
